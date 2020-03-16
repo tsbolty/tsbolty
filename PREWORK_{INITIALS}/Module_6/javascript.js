30c7717086fc659e8a06c64a168cddddcb0b4899
tsbolty <59981106+tsbolty@users.noreply.github.com>
2020-03-16 14:45:22 -0600

    Add files via upload

---
+++ PREWORK_{INITIALS}/Module_6/javascript.js
@@ -0,0 +1,16 @@
+function myFunctionGrow() {
+    document.getElementById("box").style.height = "300px";
+    document.getElementById("box").style.width = "300px";
+  }
+function myFunctionBlue() {
+    document.getElementById("box").style.backgroundColor = "#0C2DD6";
+}
+function myFunctionFade() {
+    document.getElementById("box").style.opacity = "0.3";
+}
+function myFunctionReset() {
+    document.getElementById("box").style.height = "150px";
+    document.getElementById("box").style.width = "150px";
+    document.getElementById("box").style.backgroundColor = "orange";
+    document.getElementById("box").style.opacity = "1.0";
+}
